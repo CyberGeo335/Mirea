1b501cfca5cb2ec9cad434063a883b20e683d6eb
CyberGeo335 <111456878+CyberGeo335@users.noreply.github.com>
2024-04-16 23:54:47 +0300

    Delete 2_step/Task_17 directory

--- 2_step/Task_17/Exercise1/StudentView.java
+++
@@ -1,7 +0,0 @@
-package Exercise1;
-
-public class StudentView {
-    public void printStudentDetails(String name, String rollNO) {
-        System.out.println("О студенте:\n    Имя: " + name + "\n    RollNO: " + rollNO);
-    }
-}
--- 2_step/Task_17/Exercise2/EmployeeView.java
+++
@@ -1,7 +0,0 @@
-package Exercise2;
-
-public class EmployeeView {
-    public void printEmployeeDetails(String name, int wages) {
-        System.out.println("Детали о сотруднике:\n    Имя: " + name + "\n    Заработная плата: " + wages);
-    }
-}
