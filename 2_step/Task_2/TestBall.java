7d1e47d0e32f69df84ee1c1381abc63140e44f73
CyberGeo335 <111456878+CyberGeo335@users.noreply.github.com>
2024-04-16 23:55:01 +0300

    Delete 2_step/Task_2 directory

--- 2_step/Task_2/TestBall.java
+++
@@ -1,8 +0,0 @@
-public class TestBall {
-    public static void main(String[] args) {
-        Ball b1 = new Ball(150, 150);
-        System.out.println(b1);
-        b1.move(25, 15);
-        System.out.println(b1);
-    }
-}
