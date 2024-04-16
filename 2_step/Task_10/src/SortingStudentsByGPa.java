42edb74afae773e3fef035cf92e87453e728a7a2
CyberGeo335 <111456878+CyberGeo335@users.noreply.github.com>
2024-04-16 23:28:29 +0300

    Delete 2_step/Task_10 directory

--- 2_step/Task_10/src/SortingStudentsByGPa.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-class SortingStudentsByGPa implements Comparator<Student>
-{
-    public int compare(Student a, Student b)
-    {
-        return a.getGPA() - b.getGPA();
-    }
-}
